21333a973ea387fb7209488b1fb0f5ab8ec878cc
yuriy_pidenko <yuriy.pidenko@gmail.com>
2024-05-06 13:32:00 +0300

    HW01

---
+++ Основы Javascript/homework01/site/script.js
@@ -0,0 +1,11 @@
+let celsius = 28;
+let fahrenheit = 0;
+
+fahrenheit = (9 / 5) * celsius + 32;
+console.log(`градусы в Цельсиях = ${celsius}`)
+console.log(`градусы в Фаренгейтач = ${fahrenheit}`)
+
+let userName = 'Юра';
+let admin = userName;
+
+console.log(admin)
